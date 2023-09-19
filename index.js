75cfa662bf1c69117f50d036bc261bf9202ba086
chidicolly11@gmail.com <chidicolly11@gmail.com>
2023-09-19 14:16:14 +0100

    backEnd ecommerce

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "https://preeminent-brigadeiros-99687f.netlify.app/",
+        origin: "https://shop-everyday.onrender.com",
     })
 );
 
